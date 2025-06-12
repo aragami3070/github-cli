b178141c4c2b204510029f05a8536a308fb885de
aragami3070 <smirnov17612@gmail.com>
2025-06-13 02:32:43 +0400

    Feat: add parse github url to 'owner/repo' format

--- src/git_utils/get_repo_info.rs
+++ src/git_utils/get_repo_info.rs
@@ -1,5 +1,23 @@
 use std::{io, process::Command};
 
+fn parse_github_url(url: &str) -> Result<String, io::Error> {
+    let parts: Vec<&str> = url
+        .trim() // Убираем пробелы
+        .trim_end_matches(".git") // Убираем .git в конце
+        .split(&['/', ':'][..]) // Делим по '/', ':'
+        .collect();
+    if parts.len() > 2 {
+        let owner = parts[parts.len() - 2];
+        let repo = parts[parts.len() - 1];
+        return Ok(format!("{owner}/{repo}"));
+    } else {
+        return Err(io::Error::new(
+            io::ErrorKind::InvalidData,
+            "Can't parse string",
+        ));
+    }
+}
+
 pub fn get_current_repo() -> Result<String, io::Error> {
     // Создаем команду для поиска ссылки на удаленный репозиторий
     let git_link = Command::new("git")
@@ -17,5 +35,6 @@ pub fn get_current_repo() -> Result<String, io::Error> {
     // Считываем результат выполнения команды в строку
     let repo_url = String::from_utf8_lossy(&git_link.stdout).trim().to_string();
 
-    return Ok(repo_url);
+	let parsed_url = parse_github_url(&repo_url)?;
+    return Ok(parsed_url);
 }
