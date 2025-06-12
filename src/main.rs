439ea70754411c02d9d2b52f21e3a52925e045f3
aragami3070 <smirnov17612@gmail.com>
2025-06-13 01:56:24 +0400

    Feat: add usage get_current_repo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+mod git_utils;
+
 fn main() {
-    println!("Hello, world!");
+	match git_utils::get_repo_info::get_current_repo() {
+		Ok(repo_url) => println!("{repo_url}"),
+		Err(e) => println!("{e}"),
+	}
 }
