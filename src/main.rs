b393133b0021fb647887857d8df88ef77cd65f0a
aragami3070 <smirnov17612@gmail.com>
2025-06-13 23:32:41 +0400

    Feat: update Err in match get_current_repo

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
+use clap::Parser;
+use std::process;
+
+mod cli_parse;
 mod git_utils;
 
+use crate::cli_parse::read_cli::Args;
+use crate::git_utils::get_repo_info::get_current_repo;
+
 fn main() {
-	match git_utils::get_repo_info::get_current_repo() {
-		Ok(repo_url) => println!("{repo_url}"),
-		Err(e) => println!("{e}"),
-	}
+    let repo_info: String = match get_current_repo() {
+        Ok(repo_url) => repo_url,
+        Err(e) => {
+            eprintln!("Error: {e}");
+            process::exit(1);
+        }
+    };
+
+    let args: Args = Args::parse();
 }
