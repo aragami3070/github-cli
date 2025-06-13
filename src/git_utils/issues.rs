f62c49398d2f49923ba25f90829b60940e8223b7
aragami3070 <smirnov17612@gmail.com>
2025-06-14 01:12:22 +0400

    Feat: add parse from 'owner/repo' format to vars

--- src/git_utils/issues.rs
+++ src/git_utils/issues.rs
@@ -1,8 +1,37 @@
+use std::{
+    io::{self, ErrorKind},
+    process,
+};
+
 use octorust::Client;
 
-use crate::cli_parse::read_cli;
+use crate::octorust::issues::Issues;
 
-pub fn get_issues_list(github_client: &Client, repo_info: &String){
+fn url_to_vars(url: &String) -> Result<(String, String), io::Error> {
+    if let Some(pos) = url.find('/') {
+        let owner = url[..pos].to_string();
+        let repo = url[pos + 1..].to_string();
+        return Ok((owner, repo));
+    } else {
+        return Err(io::Error::new(
+            ErrorKind::InvalidData,
+            "This url isn't valid",
+        ));
+    }
+}
+
+pub fn get_issues_list(github_client: &Client, repo_info: &String) {
+    let state = octorust::types::IssuesListState::Open;
+    let sort = octorust::types::IssuesListSort::Created;
+
+    let (owner, repo) = match url_to_vars(repo_info) {
+        Ok(info) => info,
+        Err(message) => {
+            eprintln!("Error: {message}");
+            process::exit(1);
+        }
+    };
+
+	println!("Owner: {owner}; Repo: {repo}");
 
 }
-    
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,9 @@ mod cli_parse;
 mod git_utils;
 
 use crate::cli_parse::read_cli::Args;
+use crate::cli_parse::read_cli::CliCommand;
 use crate::git_utils::get_repo_info::get_current_repo;
+use crate::git_utils::issues;
 
 fn main() {
     let repo_info: String = match get_current_repo() {
@@ -31,4 +33,9 @@ fn main() {
         Client::new("github-cli".to_string(), Credentials::Token(github_token))
             .expect("Failed to create Github client");
 
+    match args.command {
+        CliCommand::IssuesList => {
+            issues::get_issues_list(&github_client, &repo_info);
+        }
+    }
 }
