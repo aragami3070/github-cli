d8aa1bfff1221c111ebfce04c3447fc407477b9a
aragami3070 <smirnov17612@gmail.com>
2025-06-14 15:50:28 +0400

    Fix: borrow for state value

--- src/cli_parse/read_cli.rs
+++ src/cli_parse/read_cli.rs
@@ -34,7 +34,7 @@ pub enum CliCommand {
     },
 }
 
-pub fn set_state(state: String) -> Result<types::IssuesListState, io::Error> {
+pub fn set_state(state: &String) -> Result<types::IssuesListState, io::Error> {
     if state == "open" {
         return Ok(types::IssuesListState::Open);
     } else if state == "all" {
