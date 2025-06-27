795252ea9ccf26dcbee90ec83b65691196d234cf
aragami3070 <smirnov17612@gmail.com>
2025-06-28 03:52:28 +0400

    Feat: add create comment in git utils

--- src/git_utils/issues.rs
+++ src/git_utils/issues.rs
@@ -1,11 +1,11 @@
 use std::{
     io::{self, ErrorKind},
-    option, process,
+    process,
 };
 
 use octorust::types::{
-    self, IssuesCreateRequest, IssuesCreateRequestLabelsOneOf, IssuesUpdateRequest, State,
-    TitleOneOf,
+    self, IssuesCreateRequest, IssuesCreateRequestLabelsOneOf, IssuesUpdateRequest,
+    PullsUpdateReviewRequest, State, TitleOneOf,
 };
 use octorust::Client;
 
@@ -155,6 +155,36 @@ fn get_update_request(
         milestone: None,
         state: Some(state.clone()),
     }
+}
+
+pub async fn create_comment(
+    github_client: &Client,
+    repo_info: &String,
+    issue_number: &i64,
+    body: &String,
+) -> String {
+    let (owner, repo) = match url_to_vars(repo_info) {
+        Ok(info) => info,
+        Err(message) => {
+            eprintln!("Error: {message}");
+            process::exit(1);
+        }
+    };
+
+    let request = PullsUpdateReviewRequest { body: body.clone() };
+
+    let comment = github_client
+        .issues()
+        .create_comment(&owner.trim(), &repo.trim(), issue_number.clone(), &request)
+        .await;
+
+    return match comment {
+        Ok(_) => "Comment create successed".to_string(),
+        Err(message) => {
+            eprintln!("Error: {message}");
+            process::exit(1);
+        }
+    };
 }
 
 pub async fn close_issue(
