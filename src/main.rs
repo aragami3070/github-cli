6059bf2ea85f2a0f1931a42c0610e9561919c01e
aragami3070 <smirnov17612@gmail.com>
2025-07-01 01:36:31 +0400

    Chore: update main for new subcommand IssueCommand

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use crate::cli_parse::read_cli::set_option_string;
 use crate::cli_parse::read_cli::set_state;
 use crate::cli_parse::read_cli::Args;
 use crate::cli_parse::read_cli::CliCommand;
+use crate::cli_parse::read_cli::IssueCommand;
 use crate::git_utils::common::create_comment;
 use crate::git_utils::get_repo_info::get_current_repo;
 use crate::git_utils::issues;
@@ -42,122 +43,125 @@ async fn main() {
             .expect("Failed to create Github client");
 
     match args.command {
-
-        CliCommand::IssuesList {
-            creator,
-            assignee,
-            state,
-            labels,
-            numb_of_page,
-            iss_on_page,
-        } => {
-            let repo_info: String = get_repo();
-            let inp_state = match set_issues_list_state(&state) {
-                Ok(res) => res,
-                Err(message) => {
-                    eprintln!("Error: {message}");
-                    process::exit(1);
-                }
-            };
-
-            let list_issues = issues::get_issues_list(
-                &github_client,
-                &repo_info,
-                &creator,
-                &assignee,
-                &inp_state,
-                &labels,
-                &numb_of_page,
-                &iss_on_page,
-            )
-            .await;
-            println!(
-                "{} {} Issues from {} page:",
-                list_issues.len(),
+        CliCommand::Issue { subcommand } => match subcommand {
+            IssueCommand::List {
+                creator,
+                assignee,
                 state,
-                numb_of_page
-            );
-            println!();
-            for issue in list_issues {
-                println!("Issue{}: {};", issue.number, issue.title);
-                println!("Body: {}", issue.body);
+                labels,
+                numb_of_page,
+                iss_on_page,
+            } => {
+                let repo_info: String = get_repo();
+                let inp_state = match set_issues_list_state(&state) {
+                    Ok(res) => res,
+                    Err(message) => {
+                        eprintln!("Error: {message}");
+                        process::exit(1);
+                    }
+                };
+
+                let list_issues = issues::get_issues_list(
+                    &github_client,
+                    &repo_info,
+                    &creator,
+                    &assignee,
+                    &inp_state,
+                    &labels,
+                    &numb_of_page,
+                    &iss_on_page,
+                )
+                .await;
+                println!(
+                    "{} {} Issues from {} page:",
+                    list_issues.len(),
+                    state,
+                    numb_of_page
+                );
                 println!();
-            }
-        }
-
-        CliCommand::IssueCreate {
-            title,
-            body,
-            assignees,
-            labels,
-        } => {
-            let repo_info: String = get_repo();
-            let labels_list: Vec<String> = labels.split(",").map(|s| s.to_string()).collect();
-            let assignees_list: Vec<String> = assignees.split(",").map(|s| s.to_string()).collect();
-
-            let result = issues::create_issue(
-                &github_client,
-                &repo_info,
-                &title,
-                &body,
-                &assignees_list,
-                &labels_list,
-            )
-            .await;
-
-            println!("{result}");
-        }
-
-        CliCommand::IssueClose { number, comment } => {
-            let repo_info: String = get_repo();
-            let result = issues::close_issue(&github_client, &repo_info, &number, &comment).await;
-
-            println!("{result}");
-        }
-
-        CliCommand::IssueUpdate {
-            number,
-            title,
-            body,
-            assignees,
-            state,
-            labels,
-        } => {
-            let repo_info: String = get_repo();
-            let new_state = match set_state(&state) {
-                Ok(s) => s,
-                Err(message) => {
-                    eprintln!("Error: {message}");
-                    process::exit(1);
+                for issue in list_issues {
+                    println!("Issue{}: {};", issue.number, issue.title);
+                    println!("Body: {}", issue.body);
+                    println!();
                 }
-            };
+            }
 
-            let labels_list: Vec<String> = match labels {
-                Some(l) => l.split(",").map(|s| s.to_string()).collect(),
-                None => Vec::new(),
-            };
-            let assignees_list: Vec<String> = match assignees {
-                Some(a) => a.split(",").map(|s| s.to_string()).collect(),
-                None => Vec::new(),
-            };
+            IssueCommand::Create {
+                title,
+                body,
+                assignees,
+                labels,
+            } => {
+                let repo_info: String = get_repo();
+                let labels_list: Vec<String> = labels.split(",").map(|s| s.to_string()).collect();
+                let assignees_list: Vec<String> =
+                    assignees.split(",").map(|s| s.to_string()).collect();
 
-            let new_body: Option<&String> = set_option_string(&body);
-            let new_title: Option<&String> = set_option_string(&title);
+                let result = issues::create_issue(
+                    &github_client,
+                    &repo_info,
+                    &title,
+                    &body,
+                    &assignees_list,
+                    &labels_list,
+                )
+                .await;
 
-            let result = update_issue(
-                &github_client,
-                &repo_info,
-                &number,
-                new_title,
-                new_body,
-                &assignees_list,
-                &labels_list,
-                &new_state,
-            )
-            .await;
+                println!("{result}");
+            }
 
-            println!("{result}");
-        }
+            IssueCommand::Close { number, comment } => {
+                let repo_info: String = get_repo();
+                let result =
+                    issues::close_issue(&github_client, &repo_info, &number, &comment).await;
+
+                println!("{result}");
+            }
+
+            IssueCommand::Update {
+                number,
+                title,
+                body,
+                assignees,
+                state,
+                labels,
+            } => {
+                let repo_info: String = get_repo();
+                let new_state = match set_state(&state) {
+                    Ok(s) => s,
+                    Err(message) => {
+                        eprintln!("Error: {message}");
+                        process::exit(1);
+                    }
+                };
+
+                let labels_list: Vec<String> = match labels {
+                    Some(l) => l.split(",").map(|s| s.to_string()).collect(),
+                    None => Vec::new(),
+                };
+                let assignees_list: Vec<String> = match assignees {
+                    Some(a) => a.split(",").map(|s| s.to_string()).collect(),
+                    None => Vec::new(),
+                };
+
+                let new_body: Option<&String> = set_option_string(&body);
+                let new_title: Option<&String> = set_option_string(&title);
+
+                let result = update_issue(
+                    &github_client,
+                    &repo_info,
+                    &number,
+                    new_title,
+                    new_body,
+                    &assignees_list,
+                    &labels_list,
+                    &new_state,
+                )
+                .await;
+
+                println!("{result}");
+            }
+        },
 
         CliCommand::CreateComment { number, body } => {
             let repo_info: String = get_repo();
