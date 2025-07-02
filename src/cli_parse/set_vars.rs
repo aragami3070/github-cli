d83e2edf77d531c0e463b9e69329ce80a1dd5991
aragami3070 <smirnov17612@gmail.com>
2025-07-03 03:16:02 +0400

    Feat: add func for set repos list org type

--- src/cli_parse/set_vars.rs
+++ src/cli_parse/set_vars.rs
@@ -3,8 +3,8 @@ use std::io::ErrorKind;
 use std::process;
 
 use crate::git_utils::get_repo_info::get_current_repo;
-use octorust::types::ReposCreateInOrgRequestVisibility;
 use octorust::types::{self, State};
+use octorust::types::{ReposCreateInOrgRequestVisibility, ReposListOrgType};
 
 pub fn set_issues_list_state(state: &String) -> Result<types::IssuesListState, io::Error> {
     if state == "open" {
@@ -53,20 +53,34 @@ pub fn set_repo() -> String {
 }
 
 pub fn set_visibility(visibility: &String) -> Result<ReposCreateInOrgRequestVisibility, io::Error> {
-	if visibility == "" {
-		return Ok(ReposCreateInOrgRequestVisibility::Noop);
-	}
-	else if visibility == "private" {
-		return Ok(ReposCreateInOrgRequestVisibility::Private);
-	}
-	else if visibility == "public" {
-		return Ok(ReposCreateInOrgRequestVisibility::Public);
-	}
-	else if visibility == "internal" {
-		return Ok(ReposCreateInOrgRequestVisibility::Internal);
-	}
-	else {
-	    return Err(io::Error::new(ErrorKind::InvalidData, "Bad input. Visibility can be only '', 'public', 'private' or 'internal'"));
-	}
-	
+    if visibility == "" {
+        return Ok(ReposCreateInOrgRequestVisibility::Noop);
+    } else if visibility == "private" {
+        return Ok(ReposCreateInOrgRequestVisibility::Private);
+    } else if visibility == "public" {
+        return Ok(ReposCreateInOrgRequestVisibility::Public);
+    } else if visibility == "internal" {
+        return Ok(ReposCreateInOrgRequestVisibility::Internal);
+    } else {
+        return Err(io::Error::new(
+            ErrorKind::InvalidData,
+            "Bad input. Visibility can be only '', 'public', 'private' or 'internal'",
+        ));
+    }
+}
+
+pub fn set_repos_list_org_type(type_value: &String) -> Result<ReposListOrgType, io::Error> {
+    return match type_value.as_str() {
+		"all" => Ok(ReposListOrgType::All),
+		"forks" => Ok(ReposListOrgType::Forks),
+		"internal" => Ok(ReposListOrgType::Internal),
+		"member" => Ok(ReposListOrgType::Member),
+		"private" => Ok(ReposListOrgType::Private),
+		"public" => Ok(ReposListOrgType::Public),
+		"sources" => Ok(ReposListOrgType::Sources),
+		_ => Err(io::Error::new(
+            ErrorKind::InvalidData,
+            "Bad input. Type can be only 'all', 'forks', 'internal', 'member', 'private', 'public' or 'sources'",
+        )),
+	};
 }
