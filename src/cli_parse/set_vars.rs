a45f2c278cb4d138efac82c4d134f9519dd62183
aragami3070 <smirnov17612@gmail.com>
2025-07-03 03:32:01 +0400

    Chore: rewrite all if/else to match in set_vars mod

--- src/cli_parse/set_vars.rs
+++ src/cli_parse/set_vars.rs
@@ -7,39 +7,33 @@ use octorust::types::{self, State};
 use octorust::types::{ReposCreateInOrgRequestVisibility, ReposListOrgType};
 
 pub fn set_issues_list_state(state: &String) -> Result<types::IssuesListState, io::Error> {
-    if state == "open" {
-        return Ok(types::IssuesListState::Open);
-    } else if state == "all" {
-        return Ok(types::IssuesListState::All);
-    } else if state == "closed" {
-        return Ok(types::IssuesListState::Closed);
-    } else {
-        return Err(io::Error::new(
+    return match state.trim() {
+        "open" => Ok(types::IssuesListState::Open),
+        "closed" => Ok(types::IssuesListState::Closed),
+        "all" => Ok(types::IssuesListState::All),
+        _ => Err(io::Error::new(
             io::ErrorKind::InvalidData,
             "Bad input. State can be only 'open', 'all', 'closed'",
-        ));
-    }
+        )),
+    };
 }
 
 pub fn set_state(state: &String) -> Result<State, io::Error> {
-    if state == "open" {
-        return Ok(State::Open);
-    } else if state == "closed" {
-        return Ok(State::Closed);
-    } else {
-        return Err(io::Error::new(
+    return match state.trim() {
+        "open" => Ok(State::Open),
+        "closed" => Ok(State::Closed),
+        _ => Err(io::Error::new(
             io::ErrorKind::InvalidData,
             "Bad input. State can be only 'open' or 'closed'",
-        ));
-    }
+        )),
+    };
 }
 
 pub fn set_option_string(some_string: &String) -> Option<&String> {
-    if some_string == "None" {
-        return None;
-    } else {
-        return Some(some_string);
-    }
+    return match some_string.trim() {
+        "None" => None,
+        _ => Some(some_string),
+    };
 }
 
 pub fn set_repo() -> String {
@@ -53,24 +47,20 @@ pub fn set_repo() -> String {
 }
 
 pub fn set_visibility(visibility: &String) -> Result<ReposCreateInOrgRequestVisibility, io::Error> {
-    if visibility == "" {
-        return Ok(ReposCreateInOrgRequestVisibility::Noop);
-    } else if visibility == "private" {
-        return Ok(ReposCreateInOrgRequestVisibility::Private);
-    } else if visibility == "public" {
-        return Ok(ReposCreateInOrgRequestVisibility::Public);
-    } else if visibility == "internal" {
-        return Ok(ReposCreateInOrgRequestVisibility::Internal);
-    } else {
-        return Err(io::Error::new(
+    return match visibility.trim() {
+        "" => Ok(ReposCreateInOrgRequestVisibility::Noop),
+        "private" => Ok(ReposCreateInOrgRequestVisibility::Private),
+        "public" => Ok(ReposCreateInOrgRequestVisibility::Public),
+        "internal" => Ok(ReposCreateInOrgRequestVisibility::Internal),
+        _ => Err(io::Error::new(
             ErrorKind::InvalidData,
             "Bad input. Visibility can be only '', 'public', 'private' or 'internal'",
-        ));
-    }
+        )),
+    };
 }
 
 pub fn set_repos_list_org_type(type_value: &String) -> Result<ReposListOrgType, io::Error> {
-    return match type_value.as_str() {
+    return match type_value.trim() {
 		"all" => Ok(ReposListOrgType::All),
 		"forks" => Ok(ReposListOrgType::Forks),
 		"internal" => Ok(ReposListOrgType::Internal),
