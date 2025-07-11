ccc403ad8b01f6ab302e6ef7775b4b7d4067cdac
aragami3070 <smirnov17612@gmail.com>
2025-07-11 22:14:17 +0400

    Feat: add handle comment create function

--- src/cli_parse/handle_commands/handle_comment.rs
+++ src/cli_parse/handle_commands/handle_comment.rs
@@ -9,16 +9,21 @@ use crate::git_utils::repo_info::RepoInfo;
 pub async fn handle_comment_command(github_client: Client, subcommand: CommentCommand) {
     match subcommand {
         CommentCommand::Create { number, body } => {
-            let repo_info: RepoInfo = match RepoInfo::new(Repo::Current, None, None) {
-                Ok(rep) => rep,
-                Err(message) => {
-                    eprintln!("Error: {message}");
-                    process::exit(1);
-                }
-            };
-            let result = comments::create(&github_client, &repo_info, &number, &body).await;
-
-            println!("{result}");
+            handle_create(github_client, number, body).await;
         }
     };
+}
+
+async fn handle_create(github_client: Client, number: i64, body: String) {
+    let repo_info: RepoInfo = match RepoInfo::new(Repo::Current, None, None) {
+        Ok(rep) => rep,
+        Err(message) => {
+            eprintln!("Error: {message}");
+            process::exit(1);
+        }
+    };
+
+    let result = comments::create(&github_client, &repo_info, &number, &body).await;
+
+    println!("{result}");
 }
