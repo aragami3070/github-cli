7cce3dfb4f31890ef6091f3d06d39edcee13770e
aragami3070 <smirnov17612@gmail.com>
2025-07-11 16:31:19 +0400

    Feat: add handle issue update function

--- src/cli_parse/handle_commands/handle_issue.rs
+++ src/cli_parse/handle_commands/handle_issue.rs
@@ -3,10 +3,59 @@ use std::process;
 
 use crate::cli_in::issue_command::IssueCommand;
 use crate::cli_in::set_vars::IssuesListStates;
+use crate::cli_in::set_vars::States;
 use crate::cli_out::print_in_cli::print_issues;
 use crate::git_utils::issues;
 use crate::git_utils::repo_info::Repo;
 use crate::git_utils::repo_info::RepoInfo;
+
+pub async fn handle_issue_command(github_client: Client, subcommand: IssueCommand) {
+    match subcommand {
+        IssueCommand::List {
+            creator,
+            assignee,
+            state,
+            labels,
+            numb_of_page,
+            iss_on_page,
+        } => {
+            handle_list(
+                github_client,
+                creator,
+                assignee,
+                state,
+                labels,
+                numb_of_page,
+                iss_on_page,
+            )
+            .await;
+        }
+
+        IssueCommand::Create {
+            title,
+            body,
+            assignees,
+            labels,
+        } => {
+            handle_create(github_client, title, body, assignees, labels).await;
+        }
+
+        IssueCommand::Close { number, comment } => {
+            handle_close(github_client, number, comment).await;
+        }
+
+        IssueCommand::Update {
+            number,
+            title,
+            body,
+            assignees,
+            state,
+            labels,
+        } => {
+            handle_update(github_client, title, body, number, labels, assignees, state).await;
+        }
+    }
+}
 
 async fn handle_list(
     github_client: Client,
@@ -83,79 +132,43 @@ async fn handle_close(github_client: Client, number: i64, comment: String) {
     println!("{result}");
 }
 
-pub async fn handle_issue_command(github_client: Client, subcommand: IssueCommand) {
-    match subcommand {
-        IssueCommand::List {
-            creator,
-            assignee,
-            state,
-            labels,
-            numb_of_page,
-            iss_on_page,
-        } => {
-            handle_list(
-                github_client,
-                creator,
-                assignee,
-                state,
-                labels,
-                numb_of_page,
-                iss_on_page,
-            )
-            .await;
+async fn handle_update(
+    github_client: Client,
+    title: String,
+    body: String,
+    number: i64,
+    labels: Option<String>,
+    assignees: Option<String>,
+    state: States,
+) {
+    let repo_info: RepoInfo = match RepoInfo::new(Repo::Current, None, None) {
+        Ok(rep) => rep,
+        Err(message) => {
+            eprintln!("Error: {message}");
+            process::exit(1);
         }
+    };
 
-        IssueCommand::Create {
-            title,
-            body,
-            assignees,
-            labels,
-        } => {
-            handle_create(github_client, title, body, assignees, labels).await;
-        }
+    let labels_list: Vec<String> = match labels {
+        Some(l) => l.split(",").map(|s| s.to_string()).collect(),
+        None => Vec::new(),
+    };
+    let assignees_list: Vec<String> = match assignees {
+        Some(a) => a.split(",").map(|s| s.to_string()).collect(),
+        None => Vec::new(),
+    };
 
-        IssueCommand::Close { number, comment } => {
-            handle_close(github_client, number, comment).await;
-        }
+    let result = issues::update(
+        &github_client,
+        repo_info,
+        &number,
+        Some(title),
+        Some(body),
+        &assignees_list,
+        &labels_list,
+        &state.0,
+    )
+    .await;
 
-        IssueCommand::Update {
-            number,
-            title,
-            body,
-            assignees,
-            state,
-            labels,
-        } => {
-            let repo_info: RepoInfo = match RepoInfo::new(Repo::Current, None, None) {
-                Ok(rep) => rep,
-                Err(message) => {
-                    eprintln!("Error: {message}");
-                    process::exit(1);
-                }
-            };
-
-            let labels_list: Vec<String> = match labels {
-                Some(l) => l.split(",").map(|s| s.to_string()).collect(),
-                None => Vec::new(),
-            };
-            let assignees_list: Vec<String> = match assignees {
-                Some(a) => a.split(",").map(|s| s.to_string()).collect(),
-                None => Vec::new(),
-            };
-
-            let result = issues::update(
-                &github_client,
-                repo_info,
-                &number,
-                Some(title),
-                Some(body),
-                &assignees_list,
-                &labels_list,
-                &state.0,
-            )
-            .await;
-
-            println!("{result}");
-        }
-    }
+    println!("{result}");
 }
