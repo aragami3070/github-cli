55e8350b7a3b142c67d0f87b5e708d59f3c16ab4
aragami3070 <smirnov17612@gmail.com>
2025-07-12 00:09:22 +0400

    Feat: add handle repo get all from user function

--- src/cli_parse/handle_commands/handle_repo.rs
+++ src/cli_parse/handle_commands/handle_repo.rs
@@ -5,6 +5,7 @@ use crate::cli_in::repo_command::RepoCommand;
 use crate::cli_in::set_vars::Orders;
 use crate::cli_in::set_vars::ReposListOrgSorts;
 use crate::cli_in::set_vars::ReposListOrgTypes;
+use crate::cli_in::set_vars::ReposListUserTypes;
 use crate::cli_in::set_vars::Visibilities;
 use crate::cli_out::print_in_cli::print_repos;
 use crate::cli_out::print_in_cli::print_url;
@@ -12,6 +13,139 @@ use crate::git_utils::repo_info::Repo;
 use crate::git_utils::repo_info::RepoInfo;
 use crate::git_utils::repo_info::{RepoName, RepoOwner};
 use crate::git_utils::repos;
+
+pub async fn handle_repo_command(github_client: Client, subcommand: RepoCommand) {
+    match subcommand {
+        RepoCommand::CreateForAuthenticatedUser {
+            allow_auto_merge,
+            allow_merge_commit,
+            allow_rebase_merge,
+            allow_squash_merge,
+            auto_init,
+            delete_branch_on_merge,
+            description,
+            gitignore_template,
+            has_issues,
+            has_projects,
+            has_wiki,
+            homepage,
+            is_template,
+            license_template,
+            name,
+            private,
+        } => {
+            handle_create_for_auth_user(
+                github_client,
+                allow_auto_merge,
+                allow_merge_commit,
+                allow_rebase_merge,
+                allow_squash_merge,
+                auto_init,
+                delete_branch_on_merge,
+                description,
+                gitignore_template,
+                has_issues,
+                has_projects,
+                has_wiki,
+                homepage,
+                is_template,
+                license_template,
+                name,
+                private,
+            )
+            .await;
+        }
+
+        RepoCommand::CreateInOrg {
+            allow_auto_merge,
+            allow_merge_commit,
+            allow_rebase_merge,
+            allow_squash_merge,
+            auto_init,
+            delete_branch_on_merge,
+            description,
+            gitignore_template,
+            has_issues,
+            has_projects,
+            has_wiki,
+            homepage,
+            is_template,
+            license_template,
+            name,
+            org,
+            team_name,
+            visibility,
+        } => {
+            handle_create_in_org(
+                github_client,
+                allow_auto_merge,
+                allow_merge_commit,
+                allow_rebase_merge,
+                allow_squash_merge,
+                auto_init,
+                delete_branch_on_merge,
+                description,
+                gitignore_template,
+                has_issues,
+                has_projects,
+                has_wiki,
+                homepage,
+                is_template,
+                license_template,
+                name,
+                org,
+                team_name,
+                visibility,
+            )
+            .await;
+        }
+
+        RepoCommand::GetAllFromOrg {
+            org,
+            order,
+            sort_value,
+            type_value,
+        } => {
+            handle_get_all_from_org(github_client, org, order, type_value, sort_value).await;
+        }
+
+        RepoCommand::CreateUsingTemplate {
+            template_owner,
+            template_name,
+            name,
+            owner,
+            description,
+            private,
+            include_all_branches,
+        } => {
+            handle_create_using_template(
+                github_client,
+                owner,
+                name,
+                template_owner,
+                template_name,
+                description,
+                include_all_branches,
+                private,
+            )
+            .await;
+        }
+
+        RepoCommand::CreateFork { org, name, owner } => {
+            handle_create_fork(github_client, owner, name, org).await;
+        }
+
+        RepoCommand::GetAllFromUser {
+            owner,
+            order,
+            sort_value,
+            type_value,
+        } => {
+            handle_get_all_from_user(github_client, owner, type_value, sort_value, order).await;
+        }
+    }
+}
+
 
 async fn handle_create_for_auth_user(
     github_client: Client,
@@ -175,143 +309,21 @@ async fn handle_create_fork(github_client: Client, owner: RepoOwner, name: RepoN
     println!("{result}");
 }
 
-pub async fn handle_repo_command(github_client: Client, subcommand: RepoCommand) {
-    match subcommand {
-        RepoCommand::CreateForAuthenticatedUser {
-            allow_auto_merge,
-            allow_merge_commit,
-            allow_rebase_merge,
-            allow_squash_merge,
-            auto_init,
-            delete_branch_on_merge,
-            description,
-            gitignore_template,
-            has_issues,
-            has_projects,
-            has_wiki,
-            homepage,
-            is_template,
-            license_template,
-            name,
-            private,
-        } => {
-            handle_create_for_auth_user(
-                github_client,
-                allow_auto_merge,
-                allow_merge_commit,
-                allow_rebase_merge,
-                allow_squash_merge,
-                auto_init,
-                delete_branch_on_merge,
-                description,
-                gitignore_template,
-                has_issues,
-                has_projects,
-                has_wiki,
-                homepage,
-                is_template,
-                license_template,
-                name,
-                private,
-            )
-            .await;
-        }
+async fn handle_get_all_from_user(
+    github_client: Client,
+    owner: String,
+    type_value: ReposListUserTypes,
+    sort_value: ReposListOrgSorts,
+    order: Orders,
+) {
+    let result = repos::get_all_from_user(
+        &github_client,
+        owner.clone(),
+        type_value.0,
+        sort_value.0,
+        order.0,
+    )
+    .await;
 
-        RepoCommand::CreateInOrg {
-            allow_auto_merge,
-            allow_merge_commit,
-            allow_rebase_merge,
-            allow_squash_merge,
-            auto_init,
-            delete_branch_on_merge,
-            description,
-            gitignore_template,
-            has_issues,
-            has_projects,
-            has_wiki,
-            homepage,
-            is_template,
-            license_template,
-            name,
-            org,
-            team_name,
-            visibility,
-        } => {
-            handle_create_in_org(
-                github_client,
-                allow_auto_merge,
-                allow_merge_commit,
-                allow_rebase_merge,
-                allow_squash_merge,
-                auto_init,
-                delete_branch_on_merge,
-                description,
-                gitignore_template,
-                has_issues,
-                has_projects,
-                has_wiki,
-                homepage,
-                is_template,
-                license_template,
-                name,
-                org,
-                team_name,
-                visibility,
-            )
-            .await;
-        }
-
-        RepoCommand::GetAllFromOrg {
-            org,
-            order,
-            sort_value,
-            type_value,
-        } => {
-            handle_get_all_from_org(github_client, org, order, type_value, sort_value).await;
-        }
-
-        RepoCommand::CreateUsingTemplate {
-            template_owner,
-            template_name,
-            name,
-            owner,
-            description,
-            private,
-            include_all_branches,
-        } => {
-            handle_create_using_template(
-                github_client,
-                owner,
-                name,
-                template_owner,
-                template_name,
-                description,
-                include_all_branches,
-                private,
-            )
-            .await;
-        }
-
-        RepoCommand::CreateFork { org, name, owner } => {
-            handle_create_fork(github_client, owner, name, org).await;
-        }
-
-        RepoCommand::GetAllFromUser {
-            owner,
-            order,
-            sort_value,
-            type_value,
-        } => {
-            let result = repos::get_all_from_user(
-                &github_client,
-                owner.clone(),
-                type_value.0,
-                sort_value.0,
-                order.0,
-            )
-            .await;
-
-            print_repos(result, owner, "user");
-        }
-    }
+    print_repos(result, owner, "user");
 }
