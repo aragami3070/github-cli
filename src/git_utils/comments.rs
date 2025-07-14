6721dcb03683e3616ec69fae2a27688b73b58c60
aragami3070 <smirnov17612@gmail.com>
2025-07-15 01:29:33 +0400

    Feat: add get all func for comments module
    Get all Comments for issue/pull request without review comments

--- src/git_utils/comments.rs
+++ src/git_utils/comments.rs
@@ -1,6 +1,6 @@
 use std::process;
 
-use octorust::types::PullsUpdateReviewRequest;
+use octorust::types::{IssueComment, PullsUpdateReviewRequest};
 use octorust::Client;
 
 use crate::git_utils::repo_info::RepoInfo;
@@ -25,6 +25,31 @@ pub async fn create(
 
     return match comment {
         Ok(_) => "Comment create successed".to_string(),
+        Err(message) => {
+            eprintln!("Error: {message}");
+            process::exit(1);
+        }
+    };
+}
+
+// Get all Comments for issue/pull request without review comments
+pub async fn get_all(
+    github_client: &Client,
+    repo_info: &RepoInfo,
+    issue_number: &i64,
+) -> Vec<IssueComment> {
+    let list_comments = github_client
+        .issues()
+        .list_all_comments(
+            &repo_info.get_owner(),
+            &repo_info.get_name(),
+            issue_number.clone(),
+            None,
+        )
+        .await;
+
+    return match list_comments {
+        Ok(c) => c.body,
         Err(message) => {
             eprintln!("Error: {message}");
             process::exit(1);
