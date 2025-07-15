04b4ef2d2df16e8fd5ef7c130e74ebf84d58255b
aragami3070 <smirnov17612@gmail.com>
2025-07-16 02:59:27 +0400

    Feat: add flag for select get comments from issue or pull request for get-all command

--- src/cli_in/comment_command.rs
+++ src/cli_in/comment_command.rs
@@ -1,5 +1,7 @@
 use clap::Subcommand;
 
+use crate::cli_in::set_vars::CommentTarget;
+
 #[derive(Subcommand)]
 pub enum CommentCommand {
     /// Create new comment for issue/pull request
@@ -17,5 +19,8 @@ pub enum CommentCommand {
         /// Get all comments from issue/pull request with number
         #[clap(long, short)]
         number: i64,
+        /// Get from issue or from pull request (can be only 'issue' or 'pull-request')
+        #[clap(long, short)]
+        target: CommentTarget,
     },
 }
