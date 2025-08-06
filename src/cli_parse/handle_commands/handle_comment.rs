55678dc2fc254a9d31719db3c10ab7f42a042504
aragami3070 <smirnov17612@gmail.com>
2025-08-07 00:44:45 +0400

    Feat: add handle_geat_all_from_review func for handle_comment mod

--- src/cli_parse/handle_commands/handle_comment.rs
+++ src/cli_parse/handle_commands/handle_comment.rs
@@ -1,13 +1,11 @@
 use octorust::{self, Client};
 use std::process;
-// use std::str::FromStr;
 
 use crate::cli_in::comment_command::CommentCommand;
-use crate::cli_in::set_vars::CommentTarget;
+use crate::cli_in::set_vars::{CommentTarget, Orders, Sorts};
 use crate::cli_out::print_in_cli::{print_comments, print_review_comments};
 use crate::git_utils::comments;
 use crate::git_utils::repo_info::{Repo, RepoInfo};
-// use crate::git_utils::repo_info::{RepoName, RepoOwner};
 
 pub async fn handle_comment_command(github_client: Client, subcommand: CommentCommand) {
     match subcommand {
@@ -64,4 +62,24 @@ async fn handle_get_all(github_client: Client, number: i64, target: CommentTarge
     if !review_comments.is_empty() {
         print_review_comments(review_comments);
     }
+}
+
+async fn handle_get_all_from_review(
+    github_client: Client,
+    number: i64,
+    sort: Sorts,
+    order: Orders,
+) {
+    let repo_info: RepoInfo = match RepoInfo::new(Repo::Current, None, None) {
+        Ok(rep) => rep,
+        Err(message) => {
+            eprintln!("Error: {message}");
+            process::exit(1);
+        }
+    };
+
+    let result =
+        comments::get_all_from_review(&github_client, &repo_info, &number, sort.0, order.0).await;
+
+    print_review_comments(result);
 }
