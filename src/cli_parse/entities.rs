412c0354a8b1bca404ae921e78f3ba87930c4e70
aragami3070 <smirnov17612@gmail.com>
2025-08-23 20:39:54 +0400

    Feat(cli_parse): add entities mod with CreateRepoArgs struct

---
+++ src/cli_parse/entities.rs
@@ -0,0 +1,20 @@
+
+pub struct CreateRepoArgs {
+    pub allow_auto_merge: Option<bool>,
+    pub allow_merge_commit: Option<bool>,
+    pub allow_rebase_merge: Option<bool>,
+    pub allow_squash_merge: Option<bool>,
+    pub auto_init: Option<bool>,
+    pub delete_branch_on_merge: Option<bool>,
+    pub has_issues: Option<bool>,
+    pub has_projects: Option<bool>,
+    pub has_wiki: Option<bool>,
+    pub is_template: Option<bool>,
+    pub private: Option<bool>,
+    pub description: String,
+    pub gitignore_template: String,
+    pub homepage: String,
+    pub license_template: String,
+    pub name: String,
+}
+
--- src/cli_parse/mod.rs
+++ src/cli_parse/mod.rs
@@ -1,2 +1,3 @@
 pub mod handle_cli;
+pub mod entities;
 mod handle_commands;
